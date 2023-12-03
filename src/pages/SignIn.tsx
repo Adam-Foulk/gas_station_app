dccd8138073ff677af97cad9c9912eeb86dda72c
evgen_m <moruzhenko@quartsoft.com>
2023-12-03 21:07:41 +0200

    feat: user auth

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,12 @@
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { usePocketbase } from '../contexts/PocketbaseContext';
-import { FormEvent, useCallback, useRef } from 'react';
+import { FormEvent, useCallback, useEffect, useRef } from 'react';
 
 export const SignIn = () => {
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
-  const { login } = usePocketbase();
+  const { login, user } = usePocketbase();
   const navigate = useNavigate();
 
   const handleOnSubmit = useCallback(
@@ -18,6 +18,12 @@ export const SignIn = () => {
     [login],
   );
 
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user]);
+
   return (
     <section>
       <h2>Sign In</h2>
@@ -25,7 +31,6 @@ export const SignIn = () => {
         <input placeholder="Email" type="text" ref={emailRef} />
         <input placeholder="Password" type="password" ref={passwordRef} />
         <button type="submit">Login</button>
-        <Link to="/">Go to Sign Up</Link>
       </form>
     </section>
   );
